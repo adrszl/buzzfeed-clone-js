d0b632f3d2ce5ab514368ec79050c8b36a73efdd
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-06-10 21:58:25 +0200

    app.js skeleton added

---
+++ app.js
@@ -0,0 +1,28 @@
+const questionDisplay = document.querySelector('#questions');
+const answerDisplay = document.querySelector('#answer');
+const questions = [
+  {
+    id: 0,
+    text: "Pick an test answer:",
+    answers: [
+      {
+        text: "yes",
+        image: "",
+        alt: "Yes answer",
+        credit: "Image's credit"
+      },
+      {
+        text: "no",
+        image: "",
+        alt: "No answer",
+        credit: "Image's credit"
+      },
+      {
+        text: "maybe",
+        image: "",
+        alt: "Maybe answer",
+        credit: "Image's credit"
+      }
+    ]
+  }
+]
