b7f3fbb9e80fcf1758377405e26bde66ff7406fd
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-07-01 22:38:54 +0200

    questions added & method for populating questions added

--- app.js
+++ app.js
@@ -24,5 +24,37 @@ const questions = [
         credit: "Image's credit"
       }
     ]
+  },
+  {
+    id: 1,
+    text: "Pick an test answer 2:",
+    answers: [
+      {
+        text: "yes2",
+        image: "",
+        alt: "Yes answer",
+        credit: "Image's credit"
+      },
+      {
+        text: "no2",
+        image: "",
+        alt: "No answer",
+        credit: "Image's credit"
+      },
+      {
+        text: "maybe2",
+        image: "",
+        alt: "Maybe answer",
+        credit: "Image's credit"
+      }
+    ]
   }
 ]
+
+const populateQuestions = () => {
+  questions.forEach(question => {
+    const titleBlock = document.createElement('div');
+    titleBlock.id = question.id;
+    titleBlock.classList.add('title-block');
+  })
+}
